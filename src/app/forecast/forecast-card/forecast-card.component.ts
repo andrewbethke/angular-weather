70f13831298417623bca4c061ca9212b9def8bde
andrewbethke <andrew.bethke@outlook.com>
2024-07-30 22:04:58 -0500

    Added additional information to forecast cards

--- src/app/forecast/forecast-card/forecast-card.component.ts
+++ src/app/forecast/forecast-card/forecast-card.component.ts
@@ -1,14 +1,22 @@
 import { Component, Input } from '@angular/core';
 import { NWSForecastPeriod } from '../forecast.classes';
-import { NgClass } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-forecast-card',
   standalone: true,
-  imports: [NgClass],
+  imports: [NgClass, NgIf],
   templateUrl: './forecast-card.component.html',
   styleUrl: './forecast-card.component.scss'
 })
 export class ForecastCardComponent {
   @Input() period: NWSForecastPeriod = new NWSForecastPeriod();
+
+  kmToMiles(km: number){
+    return Math.round(0.6213711922 * km);
+  }
+
+  cToF(c: number) {
+    return Math.round((c * (9 / 5)) + 32);
+  }
 }
